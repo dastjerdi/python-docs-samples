af0316b7a2aa65fbfe7bea41fb6a3161259a2f31
dastjerdi <dastjerdi@college.harvard.edu>
2018-05-04 21:40:19 -0400

    a

--- language/api/doc.py
+++ language/api/doc.py
@@ -5,7 +5,6 @@ import glob
 import googleapiclient.discovery
 import os
 import concurrent.futures
-import urllib.request
 
 
 def get_native_encoding_type():
@@ -37,23 +36,24 @@ counter = 0
 results = {}
 os.chdir("texts")
 all_data = []
-for file in glob.glob("*.txt")[:5]:
+files = glob.glob("*.txt")[:3]
+for file in files:
 	with open(file, 'r') as myfile:
             data=myfile.read(999999).replace('\n', '')
             all_data += [data]
-# We can use a with statement to ensure threads are cleaned up promptly
-with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
-    # Start the load operations and mark each future with its URL
+
+data = {}
+with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
     future_to_url = {executor.submit(analyze_entities, url): url for url in all_data}
-    for future in concurrent.futures.as_completed(future_to_url):
+    for i, future in enumerate(concurrent.futures.as_completed(future_to_url)):
         url = future_to_url[future]
         try:
-            data = future.result()
+            data[files[i]] = future.result()
         except Exception as exc:
             print('%r generated an exception: %s' % (url, exc))
         else:
             print(counter)
-            counter += 1	    
+            counter += 1
 
 with open('result.json', 'w') as fp:
-    json.dump(future_to_url, fp)
+    json.dump(data, fp)
